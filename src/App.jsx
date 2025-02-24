4c8619b0cac94ec77701937249165c28a67f584d
Bruno de Oliveira <badeoliveirag@gmail.com>
2025-02-24 16:07:30 -0300

    fix #12 and finished project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
-import { gameFunctions } from "./components/game-functions";
+import { getPokemonArray } from "./components/pokemon-api";
 import { useState, useEffect} from "react";
-import { getPokemonArray } from "./components/pokemon-api"
-
-const { scoreHistory, currentScore, maxScore, randomiseCards } = gameFunctions();
 
 function App() {
   const [pokemonArray, setPokemonArray] = useState([]);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [maxScore, setMaxScore] = useState(0);
+  const [scoreHistory, setScoreHistory] = useState([]);
+  const [playedCards, setPlayedCards] = useState([]);
+
+  useEffect(() => {
+    console.log("playedCards updated:", playedCards);
+  }, [playedCards])
 
   useEffect(() => {
     getPokemonArray().then(result => {
@@ -16,16 +21,44 @@ function App() {
     });
   }, []);
 
+  useEffect(() => {
+    setMaxScore(scoreHistory.length === 0 ? 0 : Math.max(...scoreHistory));
+  }, [scoreHistory])
+  
+  const handleClick = (play) => {
+    setPlayedCards(prevPlayedCards => [...prevPlayedCards, play]);
+    const shuffled = randomiseCards(pokemonArray);
+    setPokemonArray(shuffled);
+    console.log(play);
+
+    if(!playedCards.includes(play)) {
+      setCurrentScore(prevScore => prevScore + 1)
+    } else {
+      setScoreHistory(prevHistory => [...prevHistory, currentScore]);
+      setPlayedCards([]);
+      console.log("already played", playedCards);
+    }
+  }
+
+  const randomiseCards = (array) => {
+    const newArray = array.slice();
+    for (let i = newArray.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [newArray[i], newArray[j]] = [newArray[j], newArray[i]]
+    }
+    return newArray;
+  }
+
   return (
     <>
       <div className="container">
         <div className="header">
           <div className="currentScore">current score: {currentScore} </div>
-          <div className="bestScore">best score: {maxScore(scoreHistory)}</div>
+          <div className="maxScore">best score: {maxScore}</div>
         </div>
         <div className="cards">
           {randomiseCards(pokemonArray).map((pictureUrl, index) => (
-            <div className="card" key={index}>
+            <div className="card" key={index} onClick={() => handleClick(pictureUrl)}>
               <img src={pictureUrl} alt={`Pokemon ${index + 1}`} />
             </div>
           ))}
